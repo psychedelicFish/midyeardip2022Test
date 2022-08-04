b7b7be01b81a6004418597065e67104466ab3aba
Josh <joshua.ferguson@aie.edu.au>
2022-08-05 09:22:31 +0930

    pushing some dchange

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathClasses;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Josh";
+            string t = (string)s.Reverse();
+            
+            Vector3 myVector2 = new Vector3(1,1,1);
+
+           
+
+            Console.ReadKey();
+            
+
+        }
+    }
+
+    internal class MyList<T> : List<T>
+    {
+
+        public new void Add(T item)
+        {
+
+        }
+
+
+
+    }
+}
